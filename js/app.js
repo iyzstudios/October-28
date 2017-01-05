f282da89f72a11aa211768f689ca3926c0eafe1e
iyzstudios <izzy13913@gmail.com>
2017-01-04 23:46:15 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
- $("a").click(function() {
-				$(this).addclass("animated bounce")
-			});
+
+ 
+
+$(document).ready(function(){
+    $('.center').click(function(){
+        $('#rotate-box').css({
+
+
+        //for safari & chrome
+        "-webkit-animation-name":"rotatebox",
+        "-webkit-animation-duration":"0.8s",
+        "-webkit-animation-iteration-count":"1",
+        "-webkit-animation-fill-mode" : "forwards",
+		
+        });
+        $('#fadeout').css({
+		        "-webkit-animation-name":"fadeout",
+		        "-webkit-animation-duration":"0.8s",
+		        "-webkit-animation-iteration-count":"1",
+		        "-webkit-animation-fill-mode" : "forwards",
+		        });
+    });
+})
